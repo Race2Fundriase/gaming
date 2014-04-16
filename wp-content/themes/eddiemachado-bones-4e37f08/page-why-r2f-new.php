1f27c3377abbbdcda1fcf350f6948a769652f6d1
designspin <admin@designspin.co.uk>
2014-04-16 16:20:19 +0100

    Why R2F Page
    Old Why page replaced with new design.

---
+++ wp-content/themes/eddiemachado-bones-4e37f08/page-why-r2f-new.php
@@ -0,0 +1,102 @@
+<?php get_header(); ?>
+			<section>
+            <div class="slider secondary clearfix">
+                    
+                            <img src="<?php echo get_template_directory_uri(); ?>/library/images/slide1.jpg" alt=""/>
+                    
+                    <div class="inner-slider wrap">
+                            <div id="logo" class="secondary">
+                                    <img src="<?php echo get_template_directory_uri(); ?>/library/images/r2fhomelogo.png" alt="" />
+                            </div>
+                    </div>
+                    
+            </div>
+	</section>
+	<div class="wide-fence why wrap"></div>
+	
+	<section>
+	    <div class="container pad-top sand bot-bg-grass">
+		<div class="inner-container wrap clearfix">
+                    <div>
+		    <h1 class="highlight"><?php the_title(); ?></h1>
+                    </div>
+                </div> 
+	    </div>
+	</section>
+			
+			
+			<section>
+			<div id="slide1" class="container slide-container one">
+				<div class="inner-container wrap clearfix">
+					<h2>Race2Fundraise is a unique FUNdraising idea that your supporters will enjoy playing</h2>
+					
+					<p>They get to choose racing characters and driving styles, plot courses and then when the race begins they can watch their character racing over the course, they can even pick other players to keep track of so they can enjoy the friendly competition.</p>
+				</div>
+			</div>
+			</section>
+			
+			
+			<section>
+			<div id="slide2" class="container slide-container two">
+				<div class="inner-container wrap clearfix">
+						<h2>Unique fundraising idea</h2>
+						
+						<p>You can tailor the race to exactly suit the needs of your specific cause. You determine start and end locations, start and end times, which characters you wish your supporters to choose from, you even control the weather. The race can be mimicking a real life race, creating a challenge or just be something imaginative and fun.</p>
+				</div>
+			</div>
+			</section>
+			
+			<section>
+			<div id="slide3" class="container slide-container three">
+				<div class="inner-container wrap clearfix">
+						
+						<h2>Easy to operate</h2>
+						<p>Even thought the Race2fundraising is incredibly flexible because it has a graphical user interface that makes designing a race simple and can a new race can be created in about 5 minutes. But to create a race first of all you need to register.</p>
+				
+				</div>
+			</div>
+			</section>
+			
+			<section>
+			<div id="slide4" class="container slide-container four bot-bg-alt">
+				<div class="inner-container wrap clearfix">
+					
+					<h2>Cost effective fundraising with huge fundraising potential</h2>
+					
+					<p>Race2Fundraise is a cost effective fundraising idea, you can choose to buy a small race or a larger race, you can even buy a small race and if more people are buying places than you thought you can buy additional player spaces as you need them. There is the option to purchase a single race or buy an annual subscription to operate as many races as you wish during the year. You determine the amount you wish to charge each player.<br/><br/>You can market Race2Fundraise through your digital channels and email database and take remote payment from anywhere in the world.</p>
+					
+				</div>
+			</div>
+			</section>
+			
+			<section>
+			<div class="container sand bot-bg">
+				<div class="inner-container wrap clearfix">
+					<p>You can market Race2Fundraise through your digital channels and email database and take remote payment from anywhere in the world.</p>
+					
+					<p>Race2fundraise is so flexible, you can use it to create numerous fundraising races; the only limitation is your imagination. After all for every race you can think of there is always the race back to the start!</p>
+					
+					<p>Here's some ideas of the races you can create to fundraise for your charity:-</p>
+					
+					<ul>
+						<li>Motor rally across USA</li>
+						<li>Sailing around the UK</li>
+						<li>Cycling from Land's end to John o'Groats</li>
+						<li>Camel racing to the Pyramids</li>
+						<li>Air ship race to Australia</li>
+						<li>Viking boat race to Denmark</li>
+						<li>Dog Sled race to the North Pole</li>
+						<li>Elephant racing in India</li>
+						<li>Mobility scooter racing through Ireland</li>
+						<li>Roller skating through Canada</li>
+						<li>Tuk Tuk racing across Asia</li>
+					</ul>
+					
+					<p>Race2Fundraise is fun for the players and they are being challenged and then entertained for the duration of race and they know that the price they pay to play goes to their chosen fundraising cause. In short they are receiving entertainment for their donation, meaning that they will be happy to do a different race very soon. (It's a bit better than a raffle isn't it?)</p>
+				</div>
+			</div>
+			</section>
+			
+			
+
+<?php get_footer(); ?>
