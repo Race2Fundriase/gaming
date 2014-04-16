d7b45f759697b3ea96681de78b8ea7a412f31ee9
designspin <admin@designspin.co.uk>
2014-04-16 16:24:13 +0100

    Why Page!!!!

--- wp-content/themes/eddiemachado-bones-4e37f08/page-why-r2f.php
+++ wp-content/themes/eddiemachado-bones-4e37f08/page-why-r2f.php
@@ -1,56 +1,47 @@
 <?php get_header(); ?>
 			<section>
-				<div class="slider secondary clearfix">
-					
-						<img src="<?php echo get_template_directory_uri(); ?>/library/images/slide1.jpg" alt=""/>
-					
-					<div class="inner-slider wrap">
-						<div id="logo" class="secondary">
-							<img src="<?php echo get_template_directory_uri(); ?>/library/images/r2fhomelogo.png" alt="" />
-						</div>
-					</div>
-					
-				</div>
-			</section>
-			<div class="wide-fence why wrap"></div>
-			<div id="scrolling-nav">
-				<div class="scrolling-nav-inner wrap">
-					<ul>
-						<li id="nav_slide1" class="highlight"><a href="#slide1"><span class="bullet">1</span> Fun</a></li>
-						<li id="nav_slide2" class=""><a href="#slide2"><span class="bullet">2</span> A truly unique fundraising oportunity</a></li>
-						<li id="nav_slide3" class=""><a href="#slide3"><span class="bullet">3</span> Easy to setup and administer</a></li>
-						<li id="nav_slide4" class=""><a href="#slide4"><span class="bullet">4</span> Cost effective fundraising idea</a></li>
-						<li id="nav_slide5" class=""><a href="#slide5"><span class="bullet">5</span> Inclusive to all</a></li>
-						<li id="nav_slide6" class=""><a href="#slide6"><span class="bullet">6</span> Value and entertainment for your supporters</a></li>
-					</ul>
-				</div>
-			</div>
-			<div id="scrolling-slides" class="bot-bg top-bg">
+            <div class="slider secondary clearfix">
+                    
+                            <img src="<?php echo get_template_directory_uri(); ?>/library/images/slide1.jpg" alt=""/>
+                    
+                    <div class="inner-slider wrap">
+                            <div id="logo" class="secondary">
+                                    <img src="<?php echo get_template_directory_uri(); ?>/library/images/r2fhomelogo.png" alt="" />
+                            </div>
+                    </div>
+                    
+            </div>
+	</section>
+	<div class="wide-fence why wrap"></div>
+	
+	<section>
+	    <div class="container pad-top sand bot-bg-grass">
+		<div class="inner-container wrap clearfix">
+                    <div>
+		    <h1 class="highlight"><?php the_title(); ?></h1>
+                    </div>
+                </div> 
+	    </div>
+	</section>
+			
+			
 			<section>
 			<div id="slide1" class="container slide-container one">
 				<div class="inner-container wrap clearfix">
-					<div class="title"><h1>Why Choose R2F</h1></div>
-					<div class="angle">
-						<p>The race2fundraise is fun to play, depending on how you choose to setup your race, players decide which character they want to race, select their racing colours, create their own route and determine their racing style enabling your supporters to become part of the fundraising event.</p>
-					</div>
+					<h2>Race2Fundraise is a unique FUNdraising idea that your supporters will enjoy playing</h2>
+					
+					<p>They get to choose racing characters and driving styles, plot courses and then when the race begins they can watch their character racing over the course, they can even pick other players to keep track of so they can enjoy the friendly competition.</p>
 				</div>
 			</div>
 			</section>
 			
+			
 			<section>
 			<div id="slide2" class="container slide-container two">
 				<div class="inner-container wrap clearfix">
-					<div class="angle">
-						<p>You can tailor each race to your cause or project needs.<br/>
-						You decide if the race is open to everyone (worldwide) or a closed list of invited people.<br/> 
-						You choose the race duration, start and end times.<br/>
-						You choose the race location, start and end points.<br/>
-						You set the character choice.<br/>
-						You set the player price.<br/>
-						You even control the weather.<br/>
-						You choose if there are prizes for finishing positions.
-						</p>
-					</div>
+						<h2>Unique fundraising idea</h2>
+						
+						<p>You can tailor the race to exactly suit the needs of your specific cause. You determine start and end locations, start and end times, which characters you wish your supporters to choose from, you even control the weather. The race can be mimicking a real life race, creating a challenge or just be something imaginative and fun.</p>
 				</div>
 			</div>
 			</section>
@@ -58,56 +49,54 @@
 			<section>
 			<div id="slide3" class="container slide-container three">
 				<div class="inner-container wrap clearfix">
-					<div class="angle">
-						<p>Our game engine has been designed to make it simple to setup<br/>
-						   a new race, all the game settings and reporting is<br/>
-						   available online.  Players can see their current positions<br/>
-						   online 24 hours a day.</p>
-					</div>	
+						
+						<h2>Easy to operate</h2>
+						<p>Even thought the Race2fundraising is incredibly flexible because it has a graphical user interface that makes designing a race simple and can a new race can be created in about 5 minutes. But to create a race first of all you need to register.</p>
+				
 				</div>
 			</div>
 			</section>
 			
 			<section>
-			<div id="slide4" class="container slide-container four">
+			<div id="slide4" class="container slide-container four bot-bg-alt">
 				<div class="inner-container wrap clearfix">
-					<div class="angle">
-					<p>Race2fundraise offers excellent value for money with<br/>
-					   options to reflect different supporter levels and budgets;<br/>
-					   you could even choose to buy a small race and then add<br/>
-					   extra players as you need them. You can use your own<br/>
-					   website and email system to invite people to race and<br/>
-					   fundraise.</p>
-					</div>
+					
+					<h2>Cost effective fundraising with huge fundraising potential</h2>
+					
+					<p>Race2Fundraise is a cost effective fundraising idea, you can choose to buy a small race or a larger race, you can even buy a small race and if more people are buying places than you thought you can buy additional player spaces as you need them. There is the option to purchase a single race or buy an annual subscription to operate as many races as you wish during the year. You determine the amount you wish to charge each player.<br/><br/>You can market Race2Fundraise through your digital channels and email database and take remote payment from anywhere in the world.</p>
+					
 				</div>
 			</div>
 			</section>
 			
 			<section>
-			<div id="slide5" class="container slide-container five">
+			<div class="container sand bot-bg">
 				<div class="inner-container wrap clearfix">
-					<div class="angle">
-					<p>Irrespective of age, location and agility all players can<br/>
-					   compete fairly against one another.  You can choose to make<br/>
-					   the game open to anyone online.</p>
-					</div>
+					<p>You can market Race2Fundraise through your digital channels and email database and take remote payment from anywhere in the world.</p>
+					
+					<p>Race2fundraise is so flexible, you can use it to create numerous fundraising races; the only limitation is your imagination. After all for every race you can think of there is always the race back to the start!</p>
+					
+					<p>Here's some ideas of the races you can create to fundraise for your charity:-</p>
+					
+					<ul>
+						<li>Motor rally across USA</li>
+						<li>Sailing around the UK</li>
+						<li>Cycling from Land's end to John o'Groats</li>
+						<li>Camel racing to the Pyramids</li>
+						<li>Air ship race to Australia</li>
+						<li>Viking boat race to Denmark</li>
+						<li>Dog Sled race to the North Pole</li>
+						<li>Elephant racing in India</li>
+						<li>Mobility scooter racing through Ireland</li>
+						<li>Roller skating through Canada</li>
+						<li>Tuk Tuk racing across Asia</li>
+					</ul>
+					
+					<p>Race2Fundraise is fun for the players and they are being challenged and then entertained for the duration of race and they know that the price they pay to play goes to their chosen fundraising cause. In short they are receiving entertainment for their donation, meaning that they will be happy to do a different race very soon. (It's a bit better than a raffle isn't it?)</p>
 				</div>
 			</div>
 			</section>
 			
-			<section>
-			<div id="slide6" class="container slide-container six">
-				<div class="inner-container wrap clearfix">
-					<div class="angle">
-					<p>During the race the players can see their position on the race<br/>
-					   map and compare it with their friends and family so your<br/>
-					   supporters receive entertainment for the full race<br/>
-					   duration.</p>
-					</div>
-				</div>
-			</div>
-			</section>
-			</div>
 			
 
 <?php get_footer(); ?>
