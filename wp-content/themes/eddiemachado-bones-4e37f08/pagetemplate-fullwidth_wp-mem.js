6c404407f75988525c799de80e260c4afa11ef31
designspin <admin@designspin.co.uk>
2014-04-25 14:52:15 +0100

    Profile page styled!

--- wp-content/themes/eddiemachado-bones-4e37f08/pagetemplate-fullwidth_wp-mem.js
+++ wp-content/themes/eddiemachado-bones-4e37f08/pagetemplate-fullwidth_wp-mem.js
@@ -1,4 +1,6 @@
 jQuery(document).ready(function($) {
+    
+    
     $('#main-content label').each(function() {
         text = $(this).text();
         $(this).empty();
@@ -9,11 +11,10 @@ jQuery(document).ready(function($) {
             $(this).next().andSelf().wrapAll('<div>');
     })
     
-    /*$('#main-content input').each(function() {
-        $(this).unwrap();
-        $(this).wrap('<div>');
-    })*/
-    
     $('#main-content fieldset').wrap('<div class="form-elements">');
     
+    $content = $('#main-content fieldset');
+    $content.replaceWith($content.html());
+    
+    $('#main-content input[type=submit], #main-content input[type=reset]').addClass('btn large');
 })
